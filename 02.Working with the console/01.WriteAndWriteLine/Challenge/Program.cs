fd50e72ae34975ccf02fcb323028624f5dfcb4ee
Brandon Huffman <bt_huffman@msn.com>
2020-01-22 15:31:56 -0700

    Made it to Section 3 video 21

--- 02.Working with the console/01.WriteAndWriteLine/Challenge/Program.cs
+++ 02.Working with the console/01.WriteAndWriteLine/Challenge/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
     static void Main()
     {
@@ -23,5 +25,43 @@
         System.Console.WriteLine($"{verticalSep} {one, 15}{verticalSep} " +
     $"\n{verticalSep} {two, 15}{verticalSep} " +
     $"\n{verticalSep} {three, 15}{verticalSep} ");
+
+        //SUMMARY
+        //{ 0, 10} to move 10 chars to the right with placeholders
+
+        //{ varName, 10} to move 10 chars to the right with interpolation
+
+        //{ 0/varName, 10:C5} to move and add currency rounding of 5 digits
+
+        //{ 0/varName:C5} to add currency rounding of 5 digits
+
+        ////////////////////////////////////////////////////////////////////////////////////
+        ///Accepting Single Character Inputs from the Console Read Method
+        ///
+        Console.WriteLine("How old are you? ");
+            
+        //int value = Console.Read(); //enter 15
+        //Console.WriteLine($"My age is {value}");
+        //returns age of 49 because of the ascii table. The character 1 gives you the output 49
+
+        //therefore this sb a character and save it into a char variable. Char and int are compatible so can directly type cast them as long as the integer has character representation.
+        //The read method is only reading the first character, so not quite what we want
+        //char age = (char)Console.Read(); //enter 15
+        //Console.WriteLine($"My age is {age}");
+        //returns 1 if only Console.Read else if 2nd Console.Read then 1. 
+
+        ////////////////////////////////////////////////////////////////////////////////////
+        //Accepting string inputs from the Console - readline method
+        //ReadLine returns strings instead of a single character
+        //string age2 = Console.ReadLine(); //enter 15
+        //Console.WriteLine($"My age is {age2}");
+        //returns 15
+
+        //convert or parse string so can use input as an interger (cannot typecast to integer)
+
+        //parse
+        int age3 = int.Parse(Console.ReadLine()); //enter fifteen and won't work. It will only work if use numbers
+        Console.WriteLine($"My age is {age3 + 1}");
+
     }
 }
